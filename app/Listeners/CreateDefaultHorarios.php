a3d66e24c3d5ea56e05ad487e1590bd72b869ff6
Gus <gmartinez@teiker.mx>
2024-09-10 14:37:51 -0600

    mejorando codigo

--- app/Listeners/CreateDefaultHorarios.php
+++ app/Listeners/CreateDefaultHorarios.php
@@ -20,13 +20,55 @@ class CreateDefaultHorarios
         $userId = $event->user->id;
 
         $horarios = [
-            ['user_id' => $userId, 'dia' => 'Lunes', 'abre_a' => '09:00:00', 'cierra_a' => '18:00:00', 'cerrada' => 0],
-            ['user_id' => $userId, 'dia' => 'Martes', 'abre_a' => '09:00:00', 'cierra_a' => '18:00:00', 'cerrada' => 0],
-            ['user_id' => $userId, 'dia' => 'Miércoles', 'abre_a' => '09:00:00', 'cierra_a' => '18:00:00', 'cerrada' => 0],
-            ['user_id' => $userId, 'dia' => 'Jueves', 'abre_a' => '09:00:00', 'cierra_a' => '18:00:00', 'cerrada' => 0],
-            ['user_id' => $userId, 'dia' => 'Viernes', 'abre_a' => '09:00:00', 'cierra_a' => '18:00:00', 'cerrada' => 0],
-            ['user_id' => $userId, 'dia' => 'Sábado', 'abre_a' => '09:00:00', 'cierra_a' => '18:00:00', 'cerrada' => 0],
-            ['user_id' => $userId, 'dia' => 'Domingo', 'abre_a' => '09:00:00', 'cierra_a' => '18:00:00', 'cerrada' => 1],
+            [
+                'user_id' => $userId, 
+                'dia' => 'Lunes', 
+                'abre_a' => '09:00:00', 
+                'cierra_a' => '18:00:00', 
+                'cerrada' => 0
+            ],
+            [
+                'user_id' => $userId, 
+                'dia' => 'Martes', 
+                'abre_a' => '09:00:00', 
+                'cierra_a' => '18:00:00', 
+                'cerrada' => 0
+            ],
+            [
+                'user_id' => $userId, 
+                'dia' => 'Miércoles', 
+                'abre_a' => '09:00:00', 
+                'cierra_a' => '18:00:00', 
+                'cerrada' => 0
+            ],
+            [
+                'user_id' => $userId, 
+                'dia' => 'Jueves', 
+                'abre_a' => '09:00:00', 
+                'cierra_a' => '18:00:00', 
+                'cerrada' => 0
+            ],
+            [
+                'user_id' => $userId, 
+                'dia' => 'Viernes', 
+                'abre_a' => '09:00:00', 
+                'cierra_a' => '18:00:00', 
+                'cerrada' => 0
+            ],
+            [
+                'user_id' => $userId, 
+                'dia' => 'Sábado', 
+                'abre_a' => '09:00:00', 
+                'cierra_a' => '18:00:00', 
+                'cerrada' => 0
+            ],
+            [
+                'user_id' => $userId, 
+                'dia' => 'Domingo', 
+                'abre_a' => '09:00:00', 
+                'cierra_a' => '18:00:00', 
+                'cerrada' => 1
+            ],
         ];
 
         foreach ($horarios as $horario) {
